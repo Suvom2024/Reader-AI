7bfbccf7ce0c13e19847f746d6673b4c4b96643c
soham <soham@ebiw.com>
2024-04-20 18:25:49 +0530

    Initial commit

--- app/page.tsx
+++ app/page.tsx
@@ -748,7 +748,7 @@ export default function Home() {
     <div className="iframe-container">
       <iframe
         ref={iframeRef}
-        src="/baby.html"
+        src="/Biology_IX.html"
         title="Biology Iframe"
         className="iframe-content"
         style={{ transform: `scale(${zoomLevel})` }}
